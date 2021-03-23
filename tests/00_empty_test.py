83d27fbb3e6d71ace45ca13b8decdf62909dc69e
David Sturgis <tenth10th@gmail.com>
2021-03-22 22:47:11 -0400

    Fix Issue 8
    https://github.com/pluralsight/intro-to-pytest/issues/8
    
    Add missing `tests/` to pytest command, as reported by https://github.com/elit8888

--- tests/00_empty_test.py
+++ tests/00_empty_test.py
@@ -3,7 +3,7 @@ def test_empty():
     PyTest tests are callables whose names start with "test"
     (by default)
 
-    It looks for them in modules whose name contains "test"
+    It looks for them in modules whose name starts with "test_" or ends with "_test"
     (by default)
     """
     pass
